8af54ad555deff928e48d8daf1407cdf0f7ac627
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2022-09-27 13:06:38 -0400

    add config.mk

---
+++ usr/bin/doas/config.mk
@@ -0,0 +1,27 @@
+include ../../share/mk/progs.mk
+
+PREFIX   ?=	$(PREFIX)
+EPREFIX  ?=	$(PREFIX)
+BINDIR   ?=	/bin
+SHAREDIR ?=	/usr/share
+MANDIR   ?=	$(MANPREFIX)
+SYSCONFDIR?=	/etc
+PAMDIR   ?=	/etc/pam.d
+PAM_DOAS =	pam.d__doas__linux
+BINMODE  ?=	4755
+BINOWN  ?=	root
+BINGRP  ?=	root
+OS_CFLAGS   +=	-D__linux__ -D_DEFAULT_SOURCE -D_GNU_SOURCE
+EXTRA_CPPFLAGS	+= ${HARDENING_CPPFLAGS}
+EXTRA_CFLAGS	+= ${HARDENING_CFLAGS}
+EXTRA_LDFLAGS	+= ${HARDENING_LDFLAGS}
+SRCS +=	libopenbsd/strlcat.c
+SRCS +=	libopenbsd/strlcpy.c
+SRCS +=	libopenbsd/errc.c
+SRCS +=	libopenbsd/verrc.c
+SRCS +=	libopenbsd/progname.c
+SRCS +=	libopenbsd/readpassphrase.c
+SRCS +=	libopenbsd/strtonum.c
+SRCS +=	libopenbsd/closefrom.c
+SRCS     +=	shadow.c
+LDLIBS +=	-lcrypt
