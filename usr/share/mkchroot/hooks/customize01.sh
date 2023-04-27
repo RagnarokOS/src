89df61a58029cc5d9629368678d27c684c146a47
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-04-27 15:18:59 -0400

    symlink signify-openbsd to signify

--- usr/share/mkchroot/hooks/customize01.sh
+++ usr/share/mkchroot/hooks/customize01.sh
@@ -9,6 +9,9 @@ chroot "$1" addgroup --system wheel
 # Making sure roots interactive shell is ksh
 sed -i 's/bash/ksh/g' "$1"/etc/passwd
 
+# Symlinking signify-openbsd to signify
+chroot "$1" ln -sf /usr/bin/signify-openbsd /usr/bin/signify
+
 # Needed to make the rootfs reproducible
 rm "$1"/etc/resolv.conf
 rm "$1"/etc/hostname
