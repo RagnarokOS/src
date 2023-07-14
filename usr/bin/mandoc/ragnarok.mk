f628acd27ef21458ae330c308677d76d46c61580
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-07-14 15:13:04 -0400

    added clean target

--- usr/bin/mandoc/ragnarok.mk
+++ usr/bin/mandoc/ragnarok.mk
@@ -4,7 +4,7 @@ include ../../share/mk/ragnarok-config.mk
 
 UPSTREAM_VERSION = 1.14.6
 
-all: changelog pkg
+all: changelog pkg clean
 
 changelog:
 	# use MESSAGE="message here" make -f ragnarok.mk
@@ -12,5 +12,24 @@ changelog:
 	dch --distribution ${CODENAME} -v ${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV} "${MESSAGE}"
 
 pkg:
+	# install build depends
 	mk-build-deps --install --root-cmd doas debian/control
+	# move these out of the way
+	mv mandoc-build-deps_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_all.deb ../../../../
+	mv mandoc-build-deps_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.buildinfo ../../../../
+	mv mandoc-build-deps_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.changes ../../../../
+	# Build the package
 	debuild -i -us -uc -b -rfakeroot
+
+clean:
+	#create packages dir if it doesn't exist
+	mkdir -p ../../../../packages
+	mv ../mandoc_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.deb ../../../../packages
+	rm ../mandoc-dbgsym_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.deb ../../../../packages
+	rm ../mandoc_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.build
+	rm ../mandoc_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.buildinfo
+	rm ../mandoc_${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV}_amd64.changes
+	make distclean
+	make regress-clean
+	make regress-distclean
+	dh_clean
