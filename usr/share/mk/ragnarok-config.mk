63f89892de5bb3f980ced120a66d3e799ced6480
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-07-14 16:09:08 -0400

    added TOP_DIR variable
    This will simplify the Makefiles.

--- usr/share/mk/ragnarok-config.mk
+++ usr/share/mk/ragnarok-config.mk
@@ -1,5 +1,9 @@
 # Config file to build Ragnarok packages
 
+# Directory where the repo is cloned. Needs to be writable by
+# an unprivileged user.
+TOP_DIR		= /usr/src/ragnarok
+
 DISTRO		= ragnarok
 CODENAME	= stable
 VERSION		= 01
