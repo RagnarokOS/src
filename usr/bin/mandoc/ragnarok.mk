032f3eaf09b46c0597e28371fa544ddda747aced
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-07-14 13:37:25 -0400

    add mandoc source + usr/share/mk/

---
+++ usr/bin/mandoc/ragnarok.mk
@@ -0,0 +1,17 @@
+# Ragnarok Makefile for mandoc
+
+include usr/share/mk/ragnarok-config.mk
+
+UPSTREAM_VERSION = 1.14.6
+
+all: changelog pkg
+
+changelog:
+	# use MESSAGE="message here" make -f ragnarok.mk
+	# ditto if this is a revision: MESSAGE="message" REV="number" make -f ragnarok.mk
+	dch --distribution ${CODENAME} -v ${UPSTREAM_VERSION}-${DISTRO}${VERSION}${REV} "${MESSAGE}"
+
+pkg:
+	# Don't use -b to skip building a source package. Some people may
+	# want to grab the source without cloning the whole src repo.
+	debuild -i -us -uc -rfakeroot
---
+++ usr/share/mk/ragnarok-config.mk
@@ -0,0 +1,6 @@
+# Config file to build Ragnarok packages
+
+DISTRO		= ragnarok
+CODENAME	= stable
+VERSION		= 01
+
