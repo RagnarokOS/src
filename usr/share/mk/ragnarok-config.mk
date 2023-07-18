35e14e66cfc011679d4cb3abb2c866b8df0bcde1
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-07-18 10:37:56 -0400

    Added dir paths

--- usr/share/mk/ragnarok-config.mk
+++ usr/share/mk/ragnarok-config.mk
@@ -4,6 +4,12 @@
 # an unprivileged user.
 TOP_DIR		= /usr/src/ragnarok
 
+# Path to directories within the repo
+ETC		= ${TOP_DIR}/etc
+BIN		= ${TOP_DIR}/bin
+USRBIN		= ${TOP_DIR}/usr/bin
+MAN		= ${TOP_DIR}/usr/share/man
+
 DISTRO		= ragnarok
 CODENAME	= stable
 VERSION		= 01
