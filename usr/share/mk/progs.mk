93f3223e7edd5ea625b824f2440d4cbdeb0dace7
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-07-31 19:49:08 -0400

    Changed flag order

--- usr/share/mk/progs.mk
+++ usr/share/mk/progs.mk
@@ -1,5 +1,5 @@
 # Compile time options used by programs in Ragnarok
-# $Id: progs.mk,v 1.2 2023/07/31 23:34:33 lecorbeau Exp $
+# $Id: progs.mk,v 1.3 2023/07/31 23:48:28 lecorbeau Exp $
 
 # Flags to enable ThinLTO
 CFLAGS_LTO		= -flto=thin
@@ -7,6 +7,6 @@ LDFLAGS_LTO		= -flto=thin -Wl,-O2
 
 # Hardening flags
 HARDENING_CPPFLAGS	= -D_FORTIFY_SOURCE=2
-HARDENING_CFLAGS 	= -O2 -fPIE -Wformat -Wformat-security -fstack-protector-strong \
-			  -fstack-clash-protection --param=ssp-buffer-size=4 -fcf-protection=full
+HARDENING_CFLAGS 	= -O2 -fPIE -Wformat -Wformat-security -fstack-clash-protection \
+			  -fstack-protector-strong -fcf-protection --param=ssp-buffer-size=4
 HARDENING_LDFLAGS	= -Wl,-z,relro,-z,now -Wl,-pie -Wl,-zdefs
