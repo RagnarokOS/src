fbe0194c6d2f3dfd5f9a62bb4e0daabba5cf3893
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-08-01 11:57:22 -0400

    separate -O2 from hardening flags

--- usr/share/mk/progs.mk
+++ usr/share/mk/progs.mk
@@ -1,12 +1,15 @@
 # Compile time options used by programs in Ragnarok
-# $Id: progs.mk,v 1.3 2023/07/31 23:48:28 lecorbeau Exp $
+# $Id: progs.mk,v 1.4 2023/08/01 15:56:06 lecorbeau Exp $
 
 # Flags to enable ThinLTO
 CFLAGS_LTO		= -flto=thin
 LDFLAGS_LTO		= -flto=thin -Wl,-O2
 
+# -D_FORTIFY_SOURCE=2 needs -O2 or higher.
+O_FLAG			= -O2
+
 # Hardening flags
 HARDENING_CPPFLAGS	= -D_FORTIFY_SOURCE=2
-HARDENING_CFLAGS 	= -O2 -fPIE -Wformat -Wformat-security -fstack-clash-protection \
+HARDENING_CFLAGS 	= ${O_FLAG} -fPIE -Wformat -Wformat-security -fstack-clash-protection \
 			  -fstack-protector-strong -fcf-protection --param=ssp-buffer-size=4
 HARDENING_LDFLAGS	= -Wl,-z,relro,-z,now -Wl,-pie -Wl,-zdefs
