9ebffd369438d7562ec9d918a0b7e94302b6173b
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-08-01 14:07:41 -0400

    rm ${O_FLAG} from hardening flags
    This should be set separately

--- usr/share/mk/progs.mk
+++ usr/share/mk/progs.mk
@@ -1,5 +1,5 @@
 # Compile time options used by programs in Ragnarok
-# $Id: progs.mk,v 1.5 2023/08/01 16:00:56 lecorbeau Exp $
+# $Id: progs.mk,v 1.6 2023/08/01 18:06:32 lecorbeau Exp $
 
 # Flags to enable ThinLTO
 CFLAGS_LTO		= -flto=thin
@@ -10,6 +10,6 @@ O_FLAG			= -O2
 
 # Hardening flags
 HARDENING_CPPFLAGS	= -D_FORTIFY_SOURCE=2
-HARDENING_CFLAGS 	= ${O_FLAG} -fPIE -Wformat -Wformat-security -fstack-clash-protection \
+HARDENING_CFLAGS 	= -fPIE -Wformat -Wformat-security -fstack-clash-protection \
 			  -fstack-protector-strong --param=ssp-buffer-size=4 -fcf-protection
 HARDENING_LDFLAGS	= -Wl,-z,relro,-z,now -Wl,-pie -Wl,-zdefs
