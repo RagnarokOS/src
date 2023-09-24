35bf1995d34cd0bd056e6140084c1cfbdf92f88f
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2023-09-24 12:56:54 -0400

    added fgetln and strtonum
    Prepare to add rcs

--- lib/libopenbsd/openbsd.h
+++ lib/libopenbsd/openbsd.h
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <fcntl.h>
+#include <fts.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,11 +17,57 @@
 #define __dead		__attribute__((__noreturn__))
 #endif
 
+#ifndef st_atimespec
+#define st_atimespec	st_atim
+#endif
+
+#ifndef st_ctimespec
+#define st_ctimespec	st_ctim
+#endif
+
+#ifndef st_mtimespec
+#define st_mtimespec	st_mtim
+#endif
+
+#ifndef timespecclear
+#define timespecclear(tsp)	(tsp)->tv_sec = (tsp)->tv_nsec = 0
+#endif
+
+#ifndef timespeccmp
+#define timespeccmp(tsp, usp, cmp)					\
+	(((tsp)->tv_sec == (usp)->tv_sec) ?				\
+	    ((tsp)->tv_nsec cmp (usp)->tv_nsec) :			\
+	    ((tsp)->tv_sec cmp (usp)->tv_sec))
+#endif
+
+#ifndef timespecisset
+#define timespecisset(tsp)	((tsp)->tv_sec || (tsp)->tv_nsec)
+#endif
+
+#ifndef timespecsub
+#define timespecsub(tsp, usp, vsp)					\
+	do {								\
+		(vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;		\
+		(vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;	\
+		if ((vsp)->tv_nsec < 0) {				\
+			(vsp)->tv_sec--;				\
+			(vsp)->tv_nsec += 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
+#ifndef _PW_NAME_LEN
+#define _PW_NAME_LEN	31
+#endif
+
+
+extern char	*fgetln(FILE *, size_t *);
 extern int	 getopt(int, char * const *, const char *);
 extern size_t	 strlcat(char *, const char *, size_t);
 extern size_t	 strlcpy(char *, const char *, size_t);
 extern void	*reallocarray(void *, size_t, size_t);
 extern void	*recallocarray(void *, size_t, size_t, size_t);
+extern long long strtonum(const char *, long long, long long, const char **);
 
 extern int	 opterr;
 extern int	 optind;
